91de8b1a85f5c3e075521d39b16e0f9cac312891
github-action-benchmark <github@users.noreply.github.com>
2020-10-06 17:51:17 +0000

    add Rust Benchmark (cargo) benchmark result for befdf14e4e16a23b29d5bbf74ab5e497085daf71

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602006677404,
+  "repoUrl": "https://github.com/jonasthewolf/rust-chacha",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jonas.wolf@jonaswolf.org",
+            "name": "jonasthewolf",
+            "username": "jonasthewolf"
+          },
+          "committer": {
+            "email": "jonas.wolf@jonaswolf.org",
+            "name": "jonasthewolf",
+            "username": "jonasthewolf"
+          },
+          "distinct": true,
+          "id": "befdf14e4e16a23b29d5bbf74ab5e497085daf71",
+          "message": "Default path.",
+          "timestamp": "2020-10-06T19:47:33+02:00",
+          "tree_id": "d5ccdddaf66540b983ebf1b737d9fbe60e225228",
+          "url": "https://github.com/jonasthewolf/rust-chacha/commit/befdf14e4e16a23b29d5bbf74ab5e497085daf71"
+        },
+        "date": 1602006676748,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chacha20_keystream",
+            "value": 177,
+            "range": "± 10",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
